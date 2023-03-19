d00398e27486792e8c20c7b7e7842f516b456542
Kunal Kashyap <34695022+kunalkashyap855@users.noreply.github.com>
2023-03-19 17:42:54 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 const configuration = new Configuration({
-    apiKey: "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"
+    apiKey: "YOUR API KEY"
 });
 
 const openai = new OpenAIApi(configuration);
